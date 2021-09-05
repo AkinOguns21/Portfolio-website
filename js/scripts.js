37df2ff9ce6aa1871f121afeefe7f7cc287b1b1a
AkinOguns21 <89260773+AkinOguns21@users.noreply.github.com>
2021-09-05 19:29:51 +0100

    JavaScript test on console

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,4 @@
-console.log("Hi Welcome to my porfolio site");
-var text1 = "Hello";
-var text2 = "Hello, spaces are also ok!"
-var text3 = 'Signle quotes are valid too.';
 
-var escapedText1 = "He said: \"Yes!\", that\'s for sure";
-var escapedText2 = 'he said: "Yes!", thats\'s for sure';
-
-
-console.log (text1, text2, text3);
 
 
 
@@ -21,18 +12,60 @@ var numberFloat = 50.24;
 
 
 //BOOLEANS
-
+/*
 var boolean1 = true;
 var boolean2 = false;
 var notABoolean = "false"; //this is just the string false not false
+*/
 
 
-
-console.log (thisVariableDoesntExist); //will output undefined
+//console.log (thisVariableDoesntExist); //will output undefined
 
 
 var emptyVariable = null;
 var notNullButAString = "null";
-console.log(doesntExist); // Outputs undefined
+//console.log(doesntExist); // Outputs undefined
 console.log(emptyVariable); // Outputs Null
 console.log(notNullButAString); // Outputs "null" - not the same as null!
+
+console.log("henry");
+console.log("Strings Lesson");
+console.log("__");
+
+
+
+//Edit text below
+
+console.log("Hi Welcome to my porfolio site");
+var text1 = "Hello there";
+var text2 = "Hello, spaces are also ok!";
+var text3 = 'Single quotes are valid too.';
+
+var escapedText1 = "He said: \"Yes!\", that\'s for sure";
+var escapedText2 = 'he said: "Yes!", thats\'s for sure';
+
+
+console.log(text1);
+console.log(text2, text3);
+console.log(escapedText1, escapedText2);
+
+console.log("-----------------");
+
+//NUMBERS
+
+console.log ("Numbers lesson");
+
+console.log("-------------------");
+
+console.log(numberInteger);
+console.log (numberIntegar1);
+console.log("change me to a number!");
+console.log('I\'m feeling kinda blank : (');
+
+console.log("-----------------");
+
+//var { thisVariableDoesntExist, doesntExist } = require('./helpers');
+
+console.log("Boolean lessons:");
+
+console.log("--");
