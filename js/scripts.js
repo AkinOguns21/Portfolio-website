8243980eb06c9e0948229ab745876c3e2be8be2e
AkinOguns21 <89260773+AkinOguns21@users.noreply.github.com>
2021-09-05 17:59:26 +0100

    adding variables to the console

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi Welcome to my porfolio site");
+var text1 = "Hello";
+var text2 = "Hello, spaces are also ok!"
+var text3 = 'Signle quotes are valid too.';
+
+var escapedText1 = "He said: \"Yes!\", that\'s for sure";
+var escapedText2 = 'he said: "Yes!", thats\'s for sure';
+
+
+console.log (text1, text2, text3);
